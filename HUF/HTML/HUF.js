880e371cead1d2f24c65a7fc3733b37c55f1777c
fukuirui01 <126850232+fukuirui01@users.noreply.github.com>
2023-03-03 14:53:03 +0900

    Add files via upload

---
+++ HUF/HTML/HUF.js
@@ -0,0 +1,22 @@
+$(function() {
+    // アンカーリンク
+    $('a[href^=#]').click(function() {
+        // スクロールの速度
+        var speed = 400; // ミリ秒
+        // アンカーの値取得
+        var href = $(this).attr("href");
+        // 移動先を取得
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        // 移動先を数値で取得
+        var header = $('header').height();
+        //ヘッダーの高さを引く
+        var position = target.offset().top - header;
+        // スムーススクロール
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+
+
+});
